fb8807125525f62a9afe00ae0f6bc5d8d7335f76
Neotehn <71703332+Neotehn@users.noreply.github.com>
2022-01-11 20:31:03 +0100

    navigation bar nearly done

---
+++ frauenaerzte/Javascript/navigation.js
@@ -0,0 +1,46 @@
+window.addEventListener('scroll', (event) => {
+    var pictureHeight = document.querySelector('.imgIntro').offsetHeight;
+    var navigationHeight = document.querySelector('.navigation-element').offsetHeight;
+    var scrollHeight = document.documentElement.scrollTop;
+    var navigationBar = document.getElementById("navigationBar");
+    var hamburger = document.querySelector('.toggle-button');
+    var dropDown = document.querySelector('.mobile');
+    var brand = document.querySelector('.navigation-logo');
+    var menu = document.querySelectorAll('.nav-title-mobile');
+
+
+    if (scrollHeight >= (pictureHeight - navigationHeight)) {
+        navigationBar.className = "navigation-element navigation-nontransparent";
+        dropDown.style.marginTop = "51px";
+        hamburger.style.marginTop = "5px";
+        hamburger.style.marginRight = "5px";
+        brand.style.setProperty('position', 'inherit');
+
+        for (i = 0; i < menu.length; i++) {
+            menu[i].classList.add('.menu-dropdown-beige');
+        }
+    } else {
+        navigationBar.className = "navigation-element navigation-transparent";
+        dropDown.style.marginTop = "71px";
+        hamburger.style.marginTop = "20px";
+        hamburger.style.marginRight = "20px";
+        brand.style.position = "relative";
+        brand.style.right = "-10px";
+
+        for (i = 0; i < menu.length; i++) {
+            menu[i].classList.remove('.menu-dropdown-beige');
+        }
+    }
+});
+
+function dropMenu () {
+    var menu = document.querySelector('.mobile');
+
+    if (menu.classList.contains('.active')) {
+        menu.style.display = "none";
+        menu.classList.remove('.active');
+    } else {
+        menu.style.display = "flex";
+        menu.classList.add('.active');
+    }
+}
